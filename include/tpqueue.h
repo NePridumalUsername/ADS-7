8fdc712807a90b94f38098ec7c82d7acb00fd9fd
Ivanov Vladimir <99176333+NePridumalUsername@users.noreply.github.com>
2022-05-03 14:40:19 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,10 +2,56 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
+template<class T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    struct ITEM {
+        T data;
+        ITEM *next = nullptr;
+    };
+    ITEM *head;
+    ITEM *tail;
+
+ public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+    void push(const T& data);
+    T pop();
 };
+
+template<typename T>
+void TPQueue<T>::push(const T& data) {
+    if (tail && head) {
+        ITEM *place = head;
+        while (place && (data.prior <= place->data.prior)) {
+            place = place->next;
+        }
+        if (place) {
+            ITEM *displaced = new ITEM{place->data, place->next};
+            *place = ITEM{data, displaced};
+            if (place == tail) tail = displaced;
+        } else {
+            place = new ITEM{data};
+            tail->next = place;
+            tail = place;
+        }
+    } else {
+        head = new ITEM{data};
+        tail = head;
+    }
+}
+
+template<typename T>
+T TPQueue<T>::pop() {
+    if (head) {
+        ITEM *temp = head->next;
+        T data = head->data;
+        delete head;
+        head = temp;
+        return data;
+    } else {
+        return *new T{0};
+    }
+}
 
 struct SYM {
   char ch;
